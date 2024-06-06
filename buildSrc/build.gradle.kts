d57e2a4b41fb92725a94542524e1a2b4fbd990aa
JaidynBluesky <jaidyn@blueskylabs.com.au>
2024-06-06 19:33:49 +0800

    - Updated AGP to latest 8.4.1, successful sync and build.

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -12,7 +12,7 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.20")
-    implementation("com.android.tools.build:gradle:8.2.0")
+    implementation("com.android.tools.build:gradle:8.4.1")
 }
 
 
