8cd0056c473411509ff49ba4613975b96f62feaf
JaidynBelbin <jaidyn.belbin797@gmail.com>
2024-06-08 12:29:33 +0800

    - Setup navigation utils, initial navigation from WelcomeScreen.

--- app/src/main/java/com/example/calorietracker/MainActivity.kt
+++ app/src/main/java/com/example/calorietracker/MainActivity.kt
@@ -3,6 +3,11 @@ package com.example.calorietracker
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.calorietracker.navigation.navigate
+import com.example.core.navigation.Route
 import com.example.onboarding.presentation.welcome.WelcomeScreen
 import com.plcoding.calorytracker.ui.theme.CalorieTrackerTheme
 
@@ -11,7 +16,40 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             CalorieTrackerTheme {
-                WelcomeScreen()
+                val navController = rememberNavController()
+                NavHost(navController = navController, startDestination = Route.WELCOME) {
+                    composable(Route.WELCOME) {
+                        WelcomeScreen(onNavigate = navController::navigate)
+                    }
+                    composable(Route.AGE) {
+
+                    }
+                    composable(Route.GENDER) {
+
+                    }
+                    composable(Route.HEIGHT) {
+
+                    }
+                    composable(Route.WEIGHT) {
+
+                    }
+                    composable(Route.NUTRIENT_GOAL) {
+
+                    }
+                    composable(Route.ACTIVITY) {
+
+                    }
+                    composable(Route.GOAL) {
+
+                    }
+
+                    composable(Route.TRACKER_OVERVIEW) {
+
+                    }
+                    composable(Route.SEARCH) {
+
+                    }
+                }
             }
         }
     }
